b73169651428e4db11d659358c5c62b926c27b02
Yuriy Seniuk <yoursen@gmail.com>
2024-04-03 17:19:20 +0300

    Refactor. Extract GamePageViewModel

--- WordFinder/GamePage.xaml.cs
+++ WordFinder/GamePage.xaml.cs
@@ -1,77 +1,25 @@
 namespace WordFinder;
-using CommunityToolkit.Maui.Alerts;
-using CommunityToolkit.Maui.Core;
 
 public partial class GamePage : ContentPage
 {
-    private WordsDatabase _db;
     private GamePageViewModel _viewModel;
     public GamePage(GamePageViewModel viewModel, WordsDatabase db)
     {
         InitializeComponent();
-        _db = db;
         _viewModel = viewModel;
-
-        RefreshGameField();
-
-        BindingContext = this;
-    }
-
-    private void RefreshGameField() => Letters = GreateGameField();
-
-    private GameLetter[] GreateGameField()
-    {
-        var field = new List<GameLetter>();
-        for (int i = 0; i < 25; i++)
-        {
-            var letterStr = ((char)Random.Shared.Next(65, 90)).ToString();
-            var gameLetter = new GameLetter(letterStr);
-            field.Add(gameLetter);
-        }
-        return field.ToArray();
-    }
-
-    public static readonly BindableProperty LettersProperty =
-            BindableProperty.Create(nameof(Letters), typeof(GameLetter[]), typeof(GamePage), default(GameLetter[]));
-
-    public GameLetter[] Letters
-    {
-        get => (GameLetter[])GetValue(LettersProperty);
-        set => SetValue(LettersProperty, value);
-    }
-
-    public static readonly BindableProperty GuessWordProperty =
-            BindableProperty.Create(nameof(GuessWord), typeof(string), typeof(GamePage), "");
-
-    public string GuessWord
-    {
-        get => (string)GetValue(GuessWordProperty);
-        set => SetValue(GuessWordProperty, value);
-    }
-
-    public static readonly BindableProperty UserWordProperty =
-            BindableProperty.Create(nameof(UserWord), typeof(string), typeof(GamePage), "");
-
-    public string UserWord
-    {
-        get => (string)GetValue(UserWordProperty);
-        set => SetValue(UserWordProperty, value);
+        BindingContext = _viewModel;
     }
 
     protected override async void OnNavigatedTo(NavigatedToEventArgs args)
     {
         base.OnNavigatedTo(args);
-        var gameWord = await _db.GetRandomWord();
-        GuessWord = gameWord.Description;
-        UserWord = new string('_', gameWord.Word.Length);
+        await _viewModel.Next();
     }
 
     protected override void OnNavigatingFrom(NavigatingFromEventArgs args)
     {
         base.OnNavigatingFrom(args);
-        RefreshGameField();
-        GuessWord = string.Empty;
-        UserWord = string.Empty;
+        _viewModel.Reset();
     }
 
     private async void OnBackClicked(object sender, EventArgs e) => await GoBack(sender);
@@ -93,13 +41,10 @@ public partial class GamePage : ContentPage
 
         if (frame.Parent is CharButtonView ch)
         {
-            HapticFeedback.Default.Perform(HapticFeedbackType.Click);
+            HapticFeedback.Default.Perform(HapticFeedbackType.Click);            
             ch.IsChecked = !ch.IsChecked;
 
-            if(UserWord.Contains("_"))
-                UserWord = string.Empty;
-
-            UserWord += ch.Title;
+            _viewModel.ToggleButton(ch);
         }
 
         await frame.AnimateScale();
--- WordFinder/GamePageViewModel.cs
+++ WordFinder/GamePageViewModel.cs
@@ -1,6 +1,75 @@
 namespace WordFinder;
 
-public class GamePageViewModel
+public class GamePageViewModel : BindableObject
 {
+    private WordsDatabase _db;
+    public GamePageViewModel(WordsDatabase db)
+    {
+        _db = db;
+    }
 
+    public static readonly BindableProperty LettersProperty =
+            BindableProperty.Create(nameof(Letters), typeof(GameLetter[]), typeof(GamePageViewModel), default(GameLetter[]));
+
+    public GameLetter[] Letters
+    {
+        get => (GameLetter[])GetValue(LettersProperty);
+        set => SetValue(LettersProperty, value);
+    }
+
+    public static readonly BindableProperty GuessWordProperty =
+            BindableProperty.Create(nameof(GuessWord), typeof(string), typeof(GamePageViewModel), "");
+
+    public string GuessWord
+    {
+        get => (string)GetValue(GuessWordProperty);
+        set => SetValue(GuessWordProperty, value);
+    }
+
+    public static readonly BindableProperty UserWordProperty =
+            BindableProperty.Create(nameof(UserWord), typeof(string), typeof(GamePageViewModel), "");
+
+    public string UserWord
+    {
+        get => (string)GetValue(UserWordProperty);
+        set => SetValue(UserWordProperty, value);
+    }
+
+    private void RefreshGameField() => Letters = GreateGameField();
+
+    private GameLetter[] GreateGameField()
+    {
+        var field = new List<GameLetter>();
+        for (int i = 0; i < 25; i++)
+        {
+            var letterStr = ((char)Random.Shared.Next(65, 90)).ToString();
+            var gameLetter = new GameLetter(letterStr);
+            field.Add(gameLetter);
+        }
+        return field.ToArray();
+    }
+
+    public async Task Next()
+    {
+        var gameWord = await _db.GetRandomWord();
+        GuessWord = gameWord.Description;
+        UserWord = new string('_', gameWord.Word.Length);
+
+        RefreshGameField();
+    }
+
+    public void Reset()
+    {
+        Letters = Array.Empty<GameLetter>();
+        GuessWord = string.Empty;
+        UserWord = string.Empty;
+    }
+
+    public void ToggleButton(CharButtonView ch)
+    {
+        if (UserWord.Contains("_"))
+            UserWord = string.Empty;
+
+        UserWord += ch.Title;
+    }
 }
