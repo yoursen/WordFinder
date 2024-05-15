edbb1cd055c694784668a69c06c7a7b4bbf40afb
Yuriy Seniuk <yoursen@gmail.com>
2024-05-15 22:29:25 +0300

    android specific fixes

--- WordFinder/Platforms/Android/MainActivity.cs
+++ WordFinder/Platforms/Android/MainActivity.cs
@@ -7,4 +7,11 @@ namespace WordFinder;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle bundle)
+    {
+        base.OnCreate(bundle);
+        if(DeviceInfo.Current.Idiom != DeviceIdiom.Tablet){
+            RequestedOrientation = ScreenOrientation.Portrait;
+        }
+    }
 }
