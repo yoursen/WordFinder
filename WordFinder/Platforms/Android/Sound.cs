bafe414ee2994cec12c0aaecf348765fcfab414f
Yuriy Seniuk <yoursen@gmail.com>
2024-05-17 19:09:17 +0300

    Play sound on Android

--- WordFinder/Platforms/Android/Sound.cs
+++ WordFinder/Platforms/Android/Sound.cs
@@ -1,20 +1,52 @@
+using Android.Media;
 using WordFinder.Interfaces;
 
 namespace WordFinder;
 
 public class Sound : ISound
 {
-    public bool IsPlayerReady => false;
+    public bool IsPlayerReady { get; set; }
 
-    public void Fail()
+    private MediaPlayer _mediaPlayerTap;
+    private MediaPlayer _mediaPlayerFail;
+    private MediaPlayer _mediaPlayerSuccess;
+
+    public Sound()
     {
+        var context = Android.App.Application.Context;
+
+        var fd = context.Resources.Assets.OpenFd("sounds/tap.m4a");
+        _mediaPlayerTap = new MediaPlayer();
+        _mediaPlayerTap.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
+        _mediaPlayerTap.Prepare();
+
+        fd = context.Resources.Assets.OpenFd("sounds/success.m4a");
+        _mediaPlayerSuccess = new MediaPlayer();
+        _mediaPlayerSuccess.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
+        _mediaPlayerSuccess.Prepare();
+        _mediaPlayerSuccess.SetVolume(0.4f, 0.4f);
+
+        fd = context.Resources.Assets.OpenFd("sounds/fail.m4a");
+        _mediaPlayerFail = new MediaPlayer();
+        _mediaPlayerFail.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
+        _mediaPlayerFail.Prepare();
+        _mediaPlayerFail.SetVolume(0.15f, 0.15f);
+
+        IsPlayerReady = true;
     }
 
-    public void KeyboardClick()
+    private void Play(MediaPlayer player)
     {
+        if (!IsPlayerReady)
+            return;
+
+        if (player.IsPlaying)
+            player.Stop();
+
+        player.Start();
     }
 
-    public void Success()
-    {
-    }
+    public void Fail() => Play(_mediaPlayerFail);
+    public void KeyboardClick() => Play(_mediaPlayerTap);
+    public void Success() => Play(_mediaPlayerSuccess);
 }
