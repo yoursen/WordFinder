0641ffff1610497311be8d5f02e2d49a076e317e
Yuriy Seniuk <yoursen@gmail.com>
2024-05-19 21:40:03 +0300

    fixed game startup popup async behavior

--- WordFinder/Views/GameModePopup.xaml.cs
+++ WordFinder/Views/GameModePopup.xaml.cs
@@ -6,8 +6,9 @@ namespace WordFinder.Views;
 
 public partial class GameModePopup : Popup
 {
-    private GameModePopupViewModel _viewModel;
-    private TouchFeedbackService _feedback;
+    private readonly GameModePopupViewModel _viewModel;
+    private readonly TouchFeedbackService _feedback;
+    private bool _isProgress;
     public GameModePopup(GameModePopupViewModel viewModel, TouchFeedbackService feedback)
     {
         InitializeComponent();
@@ -18,29 +19,49 @@ public partial class GameModePopup : Popup
 
     private async void On2minClicked(object sender, EventArgs e)
     {
+        if (_isProgress)
+            return;
+
+        _isProgress = true;
         _feedback.Perform();
-        await CloseAsync();
         _viewModel.StartGame(2);
+        await CloseAsync();
+        _isProgress = false;
     }
 
     private async void On5minClicked(object sender, EventArgs e)
     {
+        if (_isProgress)
+            return;
+
+        _isProgress = true;
         _feedback.Perform();
-        await CloseAsync();
         _viewModel.StartGame(5);
+        await CloseAsync();
+        _isProgress = false;
     }
 
     private async void On10minClicked(object sender, EventArgs e)
     {
+        if (_isProgress)
+            return;
+
+        _isProgress = true;
         _feedback.Perform();
-        await CloseAsync();
         _viewModel.StartGame(10);
+        await CloseAsync();
+        _isProgress = false;
     }
 
     private async void OnFreeplayClicked(object sender, EventArgs e)
     {
+        if (_isProgress)
+            return;
+
+        _isProgress = true;
         _feedback.Perform();
-        await CloseAsync();
         _viewModel.StartGame(-1);
+        await CloseAsync();
+        _isProgress = false;
     }
 }
