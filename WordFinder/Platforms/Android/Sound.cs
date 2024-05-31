115dcc88869116c9aaf2caf7f1a7423b7a4fe5d7
Yuriy Seniuk <yoursen@gmail.com>
2024-05-31 13:55:10 +0300

    Prevent crash when sound cannot be initialized

--- WordFinder/Platforms/Android/Sound.cs
+++ WordFinder/Platforms/Android/Sound.cs
@@ -15,24 +15,30 @@ public class Sound : ISound
     {
         var context = Android.App.Application.Context;
 
-        var fd = context.Resources.Assets.OpenFd("sounds/tap.m4a");
-        _mediaPlayerTap = new MediaPlayer();
-        _mediaPlayerTap.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
-        _mediaPlayerTap.Prepare();
+        try
+        {
+            var fd = context.Resources.Assets.OpenFd("sounds/tap.m4a");
+            _mediaPlayerTap = new MediaPlayer();
+            _mediaPlayerTap.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
+            _mediaPlayerTap.Prepare();
 
-        fd = context.Resources.Assets.OpenFd("sounds/success.m4a");
-        _mediaPlayerSuccess = new MediaPlayer();
-        _mediaPlayerSuccess.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
-        _mediaPlayerSuccess.Prepare();
-        _mediaPlayerSuccess.SetVolume(0.4f, 0.4f);
+            fd = context.Resources.Assets.OpenFd("sounds/success.m4a");
+            _mediaPlayerSuccess = new MediaPlayer();
+            _mediaPlayerSuccess.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
+            _mediaPlayerSuccess.Prepare();
+            _mediaPlayerSuccess.SetVolume(0.4f, 0.4f);
 
-        fd = context.Resources.Assets.OpenFd("sounds/fail.m4a");
-        _mediaPlayerFail = new MediaPlayer();
-        _mediaPlayerFail.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
-        _mediaPlayerFail.Prepare();
-        _mediaPlayerFail.SetVolume(0.15f, 0.15f);
+            fd = context.Resources.Assets.OpenFd("sounds/fail.m4a");
+            _mediaPlayerFail = new MediaPlayer();
+            _mediaPlayerFail.SetDataSource(fd.FileDescriptor, fd.StartOffset, fd.Length);
+            _mediaPlayerFail.Prepare();
+            _mediaPlayerFail.SetVolume(0.15f, 0.15f);
+            IsPlayerReady = true;
+        }
+        catch
+        {
+        }
 
-        IsPlayerReady = true;
     }
 
     private void Play(MediaPlayer player)
@@ -40,7 +46,14 @@ public class Sound : ISound
         if (!IsPlayerReady)
             return;
 
-        player.Start();
+        try
+        {
+            player.Start();
+        }
+        catch
+        {
+
+        }
     }
 
     public void Fail() => Play(_mediaPlayerFail);
